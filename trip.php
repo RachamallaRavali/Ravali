ac41bdf111818cf56e1ea7a1c46391a02cd0cedb
RachamallaRavali <129254025+RachamallaRavali@users.noreply.github.com>
2023-05-20 18:10:38 +0530

    trip.php

---
+++ trip.php
@@ -0,0 +1,89 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+    <title>Document</title>
+    <style>
+        
+        body {
+            background-image: url('https://wallpaperaccess.com/full/4863961.jpg');
+            background-repeat: no-repeat;
+            background-size: cover;
+            background-attachment: fixed;
+        }
+    </style>
+</head>
+<body>
+    
+    <nav class="navbar navbar-expand-sm navbar-light  sticky-top">
+        <div class="container-fluid">
+          <a href="#"><img src="https://rrtourism.com/images/newlogo.png" alt="" width="80" height="40" ></a>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+  <a class="nav-link" href="index.php"><b>Admin Page</b></a> 
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+<li class="nav-item">
+  <a class="nav-link" href="student-create.php"><b>Form</b></a> 
+  </li>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+  
+
+<li class="nav-item">
+<a class="nav-link" href="user.php"><b>User</b></a> 
+</li>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+
+
+</div>
+</ul>
+
+</div>
+
+</nav>
+<br>
+<center><h1 style="color:darkmagenta;">Tourism Places</h1></center>
+<footer id="footer">
+    <div class="footer-top">
+<div id="chocolate">
+<div class="container">
+ <div class="row">
+     <div class="col-4">
+         <div class="card" style="width:200">
+             <img src="https://www.dnpindia.in/wp-content/uploads/2021/01/63-Best-Tourist-Places-To-Visit-In-India-For-A-Perfect-Holiday-In-2020.jpg" alt=" " class="card-img-top"width="200" height="250">
+             <div class="card-body">
+             <center><h4 class="card-title">India</h4></center>
+             </div>
+         </div>
+     </div>
+     <div class="col-4">
+           <div class="card" style="width:200">
+               <img src="https://vacationtravelinsider.com/wp-content/uploads/2023/04/best-places-to-visit-in-may-usa.jpg" alt=" " class="card-img-top" width="280" height="250">
+               <div class="card-body">
+               <center><h4 class="card-title">USA</h4></center>
+           
+           </div>
+           </div>
+           </div>
+       <div class="col-4">
+         <div class="card" style="width:200">
+             <img src="https://previews.123rf.com/images/pinkcandy/pinkcandy1705/pinkcandy170500018/77676269-collage-of-images-from-famous-location-in-montreal-canada-with-copy-space-in-the-middle.jpg" alt=" " class="card-img-top" width="280" height="250">
+             <div class="card-body">
+             <center><h4 class="card-title">Canada</h4></center>
+         
+         </div>
+         </div>
+         </div>
+         
+           </div>
+           </div>
+           </div>
+           </div>
+          </div>
+    
+  </footer>     
+</body>
+</html>
